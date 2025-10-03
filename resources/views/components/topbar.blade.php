12598be7809a64f0cd180369f764d85b0ac7a312
OscarGPTS <ochavez@gptservices.com>
2025-10-03 14:45:18 -0600

    fix url de perfil

--- resources/views/components/topbar.blade.php
+++ resources/views/components/topbar.blade.php
@@ -37,9 +37,7 @@
                 <li>
                   <a href="{{ route('profile.index') }}" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white" role="menuitem">Mi Perfil</a>
                 </li>
-                <li>
-                  <a href="{{ route('profile.edit') }}" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white" role="menuitem">Configuración</a>
-                </li>
+          
 
                 <li>
                   <button data-modal-target="logout-modal" data-modal-toggle="logout-modal" class="w-full text-left px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 
